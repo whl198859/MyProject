c74fc505d41c7676c293ce3a94de5130bb86d29c
whl18859 <whl198859@163.com>
2015-12-09 11:57:29 +0800

    创建工程

---
+++ Exam/Exam/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Exam
+//
+//  Created by Hailong.wang on 15/12/9.
+//  Copyright © 2015年 Hailong.wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Exam/Exam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Exam
+//
+//  Created by Hailong.wang on 15/12/9.
+//  Copyright © 2015年 Hailong.wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
